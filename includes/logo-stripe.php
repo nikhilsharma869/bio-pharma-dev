16b303cbb812d03e599fa3e4c6592115509daeea
Curanh <ndthanh.it@gmail.com>
2014-08-26 16:44:13 +0700

    +Fix image broken on homepage

--- includes/logo-stripe.php
+++ includes/logo-stripe.php
@@ -1,24 +1,24 @@
 <div class="logoStrip">
     	<div id="logoStripPanel">
-    		<img id="loved_by" src="http://www.bio-pharma.com/development/wp-content/themes/bio-pharma/images/loved_by.png">
+    		<img id="loved_by" src="/images/homepage/loved_by.png">
             <div id="logos">
 	    		<ul>
-                    <li><a href="#"><img src="http://www.bio-pharma.com/development/wp-content/themes/bio-pharma/images/logos/saic.png"></a></li>
-                    <li><a href="#"><img src="http://www.bio-pharma.com/development/wp-content/themes/bio-pharma/images/logos/nih.png"></a></li>
-                    <li><a href="#"><img src="http://www.bio-pharma.com/development/wp-content/themes/bio-pharma/images/logos/medimmune.png"></a></li>
-                    <li><a href="#"><img src="http://www.bio-pharma.com/development/wp-content/themes/bio-pharma/images/logos/bd.png"></a></li>
-                    <li><a href="#"><img src="http://www.bio-pharma.com/development/wp-content/themes/bio-pharma/images/logos/pda.png"></a></li>
-                    <li><a href="#"><img src="http://www.bio-pharma.com/development/wp-content/themes/bio-pharma/images/logos/ispe.png"></a></li>
+                    <li><a href="#"><img src="/images/homepage/logos/saic.png"></a></li>
+                    <li><a href="#"><img src="/images/homepage/logos/nih.png"></a></li>
+                    <li><a href="#"><img src="/images/homepage/logos/medimmune.png"></a></li>
+                    <li><a href="#"><img src="/images/homepage/logos/bd.png"></a></li>
+                    <li><a href="#"><img src="/images/homepage/logos/pda.png"></a></li>
+                    <li><a href="#"><img src="/images/homepage/logos/ispe.png"></a></li>
             	</ul>
             </div>
             
-            <img id="be_our_buddy" src="http://www.bio-pharma.com/development/wp-content/themes/bio-pharma/images/be-our-buddy.png">
+            <img id="be_our_buddy" src="/images/homepage/be-our-buddy.png">
             <div id="social">
 	    		<ul>
-                    <li><a href="#"><img src="http://www.bio-pharma.com/development/wp-content/themes/bio-pharma/images/rss.png"></a></li>
-                    <li><a href="#"><img src="http://www.bio-pharma.com/development/wp-content/themes/bio-pharma/images/twitter.png"></a></li>
-                    <li><a href="#"><img src="http://www.bio-pharma.com/development/wp-content/themes/bio-pharma/images/fb.png"></a></li>
-                    <li><a href="#"><img src="http://www.bio-pharma.com/development/wp-content/themes/bio-pharma/images/windows.png"></a></li>
+                    <li><a href="#"><img src="/images/homepage/rss.png"></a></li>
+                    <li><a href="#"><img src="/images/homepage/twitter.png"></a></li>
+                    <li><a href="#"><img src="/images/homepage/fb.png"></a></li>
+                    <li><a href="#"><img src="/images/homepage/windows.png"></a></li>
             	</ul>
             </div>
             
