fce1f600e59e0d8439f88ec6231802a319a16e1a
Curanh <ndthanh.it@gmail.com>
2014-09-15 23:00:25 +0700

    + Left Menu Job Client

---
+++ includes/left_menu_job_client.php
@@ -0,0 +1,6 @@
+<ul id="up-tabs" class="nav nav-tabs" role="tablist">
+	<li  class="active"><a href="<?= $vpath ?>postjob.html">Job Postings</a></li>
+	<li><a href="<?= $vpath ?>postjob.html">Post a Job</a></li>
+	<li><a href="<?= $vpath ?>postjob.html">Find Freelancers</a></li>
+	<li><a href="<?= $vpath ?>postjob.html">Saved Freelancers</a></li>
+</ul>
