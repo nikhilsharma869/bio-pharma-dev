a050d801e07597451dfbe7aa4efa743182e1f9fe
vietsmile <lqviet.it@gmail.com>
2014-10-07 14:39:13 +0700

    * up file my_jobs_sme.php

--- my_jobs_sme.php
+++ my_jobs_sme.php
@@ -19,8 +19,8 @@ include "includes/header.php";
                 <div class="profile_right">
                     <!-- content data list -->
                     <div class="content-right">
-                    <?php 
-                        $my_jobs = get_my_job('FBO:COMBINE:darpa-baa-14-28','F');
+                    <?php
+                        $my_jobs = get_my_job($_SESSION['user_id'],'F');
                         if($my_jobs!=NULL):?>                            
                             <div class="job-item">    
                                 <h4 class="job-title">Hourly</h4>
@@ -43,32 +43,29 @@ include "includes/header.php";
                                 </div>
                             </div>
                     <?php endif;?>                        
-
-                        <div class="job-item">
-                            <h4 class="job-title">Fixed Price</h4>                        
-                            <div class="job-content">
-                                <div class="box">
-                                    <div class="left-box">
-                                        <p class="name-member">SDBC - Supernova Media</p>
-                                        <p class="small-text">Hired by Raj Desai</p>
-                                        <p><a href="#">job Details</a><span class="line">|</span><a href="#">Send Message</a></p>
-                                    </div>
-                                    <div class="right-box">
-                                        <p class="time-of-price">$50.20 paid of $22.50 </p>
-                                    </div>
-                                </div>
-                                <div class="box">
-                                    <div class="left-box">
-                                        <p class="name-member">SDBC - Supernova Media</p>
-                                        <p class="small-text">Hired by Raj Desai</p>
-                                        <p><a href="#">job Details</a><span class="line">|</span><a href="#">Send Message</a></p>
-                                    </div>
-                                    <div class="right-box">
-                                        <p class="time-of-price">$50.20 paid of $22.50 </p>
-                                    </div>
+                    <?php $my_jobs2 = get_my_job($_SESSION['user_id'],'H');
+                        if($my_jobs!=NULL):?>                            
+                            <div class="job-item">    
+                                <h4 class="job-title">Hourly</h4>
+                                <div class="job-content">
+                                <?php     
+                                    foreach ($my_jobs2 as $job): {?>
+                                        <div class="box">
+                                            <div class="left-box">
+                                                <p class="name-member"><?php echo $job['project'];?></p>
+                                                <p class="small-text">Hired by <?php echo $job['username'];?></p>
+                                                <p><a href="#">job Details</a><span class="line">|</span><a href="#">Send Message</a></p>
+                                            </div>
+                                            <div class="right-box">
+                                                <p class="time-week">4:00 of 20 hrs this week</p>
+                                                <p class="small-text">@$12.00/hr = $48.00</p>
+                                                <a href="#" class="view-load">view work load</a>
+                                            </div>
+                                        </div>
+                                <?php } endforeach; ?>
                                 </div>
                             </div>
-                        </div>
+                    <?php endif;?> 
                         
                     </div>
                 </div>
