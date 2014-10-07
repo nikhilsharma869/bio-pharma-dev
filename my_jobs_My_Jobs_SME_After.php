08ad22b24f5129bb767007dc7de48f0c5408cde2
vietsmile <lqviet.it@gmail.com>
2014-10-07 09:42:56 +0700

    + add file my_jobs_My_Jobs_SME_After.php * up file biopharma.css

---
+++ my_jobs_My_Jobs_SME_After.php
@@ -0,0 +1,110 @@
+<?php
+include "includes/header.php";
+?>
+    <div class="spage-container my_jobs_My_Jobs_SME_After">
+        <div class="main_div2">
+            <div class="inner-middle"> 
+                <!-- Sidebar left -->
+                <div class="profile_left contracts_left">
+                    <!-- tabs left -->
+                    <h3 class="title-page">My Jobs</h3>
+                    <ul id="up-tabs" class="nav nav-tabs" role="tablist">
+                        <li class="active"><img class="job_icon" src="images/img/v_icon1.png" alt=""/><a href="<?= $vpath ?>postjob.html">My Jobs</a></li>
+                        <li><img class="job_icon" src="images/img/v_icon2.png" alt=""/><a href="<?= $vpath ?>postjob.html">Job Postings</a></li>
+                        <li><img class="job_icon" src="images/img/v_icon3.png" alt=""/><a href="<?= $vpath ?>postjob.html">Post a Job</a></li>
+                        <li><img class="job_icon" src="images/img/v_icon4.png" alt=""/><a href="<?= $vpath ?>postjob.html">Find SMEs</a></li>
+                        <li><img class="job_icon" src="images/img/v_icon5.png" alt=""/><a href="<?= $vpath ?>postjob.html">Saved SMEs</a></li>
+                    </ul>
+                </div>
+                <!-- Content right -->
+                <div class="profile_right">
+                    <!-- content data list -->
+                    <div class="content-right">
+                        <div class="job-item">
+                            <h4 class="job-title">Hourly</h4>                        
+                            <div class="job-content">
+                                <div class="box">
+                                    <div class="left-box">
+                                        <p class="name-member">Sr. Designer - myCS</p>
+                                        <p class="small-text">Hired by Raj Desai</p>
+                                        <p><a href="#">job Details</a><span class="line">|</span><a href="#">Send Message</a></p>
+                                    </div>
+                                    <div class="right-box">
+                                        <p class="time-week">4:00 of 20 hrs this week</p>
+                                        <p class="small-text">@$12.00/hr = $48.00</p>
+                                        <a href="#" class="view-load">view work load</a>
+                                    </div>
+                                </div>
+                                <div class="box">
+                                    <div class="left-box">
+                                        <p class="name-member">Sr. Designer - myCS</p>
+                                        <p class="small-text">Hired by Raj Desai</p>
+                                        <p><a href="#">job Details</a><span class="line">|</span><a href="#">Send Message</a></p>
+                                    </div>
+                                    <div class="right-box">
+                                        <p class="time-week">4:00 of 20 hrs this week</p>
+                                        <p class="small-text">@$12.00/hr = $48.00</p>
+                                        <a href="#" class="view-load">view work load</a>
+                                    </div>
+                                </div>
+                                <div class="box">
+                                    <div class="left-box">
+                                        <p class="name-member">Sr. Designer - myCS</p>
+                                        <p class="small-text">Hired by Raj Desai</p>
+                                        <p><a href="#">job Details</a><span class="line">|</span><a href="#">Send Message</a></p>
+                                    </div>
+                                    <div class="right-box">
+                                        <p class="time-week">4:00 of 20 hrs this week</p>
+                                        <p class="small-text">@$12.00/hr = $48.00</p>
+                                        <a href="#" class="view-load">view work load</a>
+                                    </div>
+                                </div>
+                                <div class="box">
+                                    <div class="left-box">
+                                        <p class="name-member">Sr. Designer - myCS</p>
+                                        <p class="small-text">Hired by Raj Desai</p>
+                                        <p><a href="#">job Details</a><span class="line">|</span><a href="#">Send Message</a></p>
+                                    </div>
+                                    <div class="right-box">
+                                        <p class="time-week">4:00 of 20 hrs this week</p>
+                                        <p class="small-text">@$12.00/hr = $48.00</p>
+                                        <a href="#" class="view-load">view work load</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="job-item">
+                            <h4 class="job-title">Fixed Price</h4>                        
+                            <div class="job-content">
+                                <div class="box">
+                                    <div class="left-box">
+                                        <p class="name-member">SDBC - Supernova Media</p>
+                                        <p class="small-text">Hired by Raj Desai</p>
+                                        <p><a href="#">job Details</a><span class="line">|</span><a href="#">Send Message</a></p>
+                                    </div>
+                                    <div class="right-box">
+                                        <p class="time-of-price">$50.20 paid of $22.50 </p>
+                                    </div>
+                                </div>
+                                <div class="box">
+                                    <div class="left-box">
+                                        <p class="name-member">SDBC - Supernova Media</p>
+                                        <p class="small-text">Hired by Raj Desai</p>
+                                        <p><a href="#">job Details</a><span class="line">|</span><a href="#">Send Message</a></p>
+                                    </div>
+                                    <div class="right-box">
+                                        <p class="time-of-price">$50.20 paid of $22.50 </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>  
+</div>
+<?php include 'includes/footer.php'; ?>
