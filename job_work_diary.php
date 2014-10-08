d5164c0b725ad47525dafa689bd834994fd2fb87
vietsmile <lqviet.it@gmail.com>
2014-10-08 10:59:07 +0700

    + add file job_work_diary.php

--- job_work_diary.php
+++ job_work_diary.php
@@ -615,7 +615,7 @@ include "includes/header.php";
                <td>c</td>
                <td>d</td>
            </tr>
-       </table>
+       </table>    
     </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
