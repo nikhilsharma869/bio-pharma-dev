d927024d1d16bb86c4ad768aa9984ed29938ca8c
Curanh <ndthanh.it@gmail.com>
2014-10-13 17:29:32 +0700

    * Update Add Manual Time

--- ajax_action.php
+++ ajax_action.php
@@ -42,3 +42,60 @@ function remove_save_sme() {
 		echo "Remove failed!";
 	}
 }
+
+function add_manual_time() {
+	global $prev;
+	$user_id = $_REQUEST['user_id'];
+	$project_id = $_REQUEST['project_id'];
+
+	$date2add = $_REQUEST['date2add'];
+	$stime2add = $_REQUEST['stime2add'];
+	$etime2add = $_REQUEST['etime2add'];
+
+	$memo2add = $_REQUEST['memo2add'];
+
+	$start_time = date('Y-m-d H:i:s', strtotime($date2add.' '.$stime2add));
+	$stop_time = date('Y-m-d H:i:s', strtotime($date2add.' '.$etime2add));
+	
+	if($start_time >= $stop_time) {
+		$ra['error'] = "Start time should not greater or equal stop time!";
+		echo json_encode($ra);
+		exit();
+	}
+
+	$q_checker = sprintf("SELECT * FROM ".$prev."project_tracker WHERE project_id='%s' AND worker_id='%s' AND ((start_time <= '%s' AND start_time >= '%s') OR (stop_time <= '%s' AND stop_time >= '%s'))",
+		mysql_real_escape_string($project_id),
+		mysql_real_escape_string($user_id),
+		mysql_real_escape_string($stop_time),
+		mysql_real_escape_string($start_time),
+		mysql_real_escape_string($stop_time),
+		mysql_real_escape_string($start_time)
+	);
+
+	$row_check = mysql_num_rows(mysql_query($q_checker));
+
+	$ra = array();
+	if($row_check > 0) {
+		$ra['error'] = "Exist Time! Please select other start time and end time!";
+	} else {
+		$q_itracker = sprintf("INSERT INTO ".$prev."project_tracker SET project_id='%s',worker_id='%s',start_time='%s',stop_time='%s',note='%s',status='1',time_added_by='M'",
+			mysql_real_escape_string($project_id),
+			mysql_real_escape_string($user_id),
+			mysql_real_escape_string($start_time),
+			mysql_real_escape_string($stop_time),
+			mysql_real_escape_string($memo2add)
+		);
+
+		$r_itracker = mysql_query($q_itracker);
+		$itracker_id=mysql_insert_id();
+		if($itracker_id) {
+			mysql_query("INSERT INTO ".$prev."project_tracker_snap SET project_tracker_id='".$itracker_id."',project_work_snap_time='".mysql_real_escape_string($start_time)."'");
+			mysql_query("INSERT INTO ".$prev."project_tracker_snap SET project_tracker_id='".$itracker_id."',project_work_snap_time='".mysql_real_escape_string($stop_time)."'");
+		}
+		$ra['success'] = "Added successfully!";
+	}
+
+	echo json_encode($ra);
+	exit();
+}
+
--- job_work_diary.php
+++ job_work_diary.php
@@ -1,7 +1,9 @@
 <?php
 include "includes/header.php";
 
-$current_date = date('m/d/Y');
+$current_date = date('D, M d, Y');
+
+$every_10_minutes = hoursRange( 0, 86400, 60 * 10, 'h:i a' );
 ?>
     <div class="spage-container job_work_diary">
         <div class="main_div2">
@@ -49,7 +51,7 @@ $current_date = date('m/d/Y');
                                 </ul>
                             </div>
                             <div class="datetime">
-                                <input type="text" id="datepicker" value='<?=$current_date?>'>
+                                <input type="text" id="datepicker" value='' class="date2add">
                                 <img class="clander-img" src="css/img/calender_icon.png" alt="">
                             </div>
                             <a href="#" class="pref">Pref</a>
@@ -599,63 +601,142 @@ $current_date = date('m/d/Y');
 </div>
 <!-- Modal -->
 <div class="modal fade job-modal" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-        <h4 class="modal-title" id="myModalLabel">Add Manual Time</h4>
-      </div>
-      <div class="modal-body">
-        <div class="m-row">
-            <label>Date</label>
-            <p>Wed,Oct 08, 2014</p>
-        </div>   
-        <div class="m-row">
-            <label>Timezone</label>
-            <p>UCT+00</p>
-        </div>
-        <div class="m-row">
-            <label>From</label>
-            <div class="m-ddrop">
-                <div class="sv-dropdown f-time">
-                    <div class="sv-dropSelect">10:30pm</div>
-                    <ul>
-						<li>12:10am</li>
-                        <li>12:20am</li>
-                        <li>12:30am</li>
-                        <li>12:40pm</li>
-                        <li>12:50pm</li>
-                        <li>01:00pm</li>
-
-                    </ul>
-                </div>
-                <span class="to">to</span>
-                <div class="sv-dropdown to-time">
-                    <div class="sv-dropSelect">11:00pm</div>
-                    <ul>
-                        <li>10:40pm</li>
-                        <li>10:50pm</li>
-                        <li>11:00pm</li>
-                        <li>11:10pm</li>
-                    </ul>
-                </div>
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                <h4 class="modal-title" id="myModalLabel">Add Manual Time</h4>
             </div>
-        </div>
-        <div class="m-row">
-            <label>Memo</label>
-            <p><textarea class="ta-memo"></textarea></p>
-            <P class="note">Note: Manual time does to quanlify for gruaranteed payment.</p>
-            <a href="#">Learn more</a>
+            <div class="modal-body">
+                <form class="form-horizontal" role="form" id="add_manual_time_f" name="add_manual_time_f">
+                    <div class="alert alert-success" role="alert">Success</div>
+                    <div class="alert alert-warning" role="alert">Error</div>
+
+                    <input type="hidden" id="project_id" value="<?=$_REQUEST['id']?>">
+                    <input type="hidden" id="user_id" value="<?=$_SESSION['user_id']?>">
+                  <div class="form-group">
+                    <label for="" class="col-sm-4 control-label">Date</label>
+                    <div class="col-sm-8">
+                       <span class="date2add"></span> 
+                       <input class="form-control" type="hidden" name="date2add" id="date2add">
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label for="" class="col-sm-4 control-label">Timezone</label>
+                    <div class="col-sm-8">
+                        <span class="timezone2add">UTC+00</span> 
+                        <input class="form-control" type="hidden" name="timezone2add" id="timezone2add">
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label for="" class="col-sm-4 control-label">From</label>
+                    <div class="col-sm-8">
+                        <div class="row">
+                          <div class="col-xs-5">                            
+                            <select class="form-control time_select_box" name="stime2add" id="stime2add">
+                                <option value="">Select</option>
+                                <?php foreach ($every_10_minutes as $key_time => $value) : ?>
+                                <option value="<?=$key_time?>"><?=$value?></option>
+                                <?php endforeach; ?>
+                            </select>                            
+                          </div>
+                          <div style="display: inline-block;float: left;padding: 3px 0; width: 15px;">To</div>
+                          <div class="col-xs-5">                            
+                            <select class="form-control time_select_box" name="etime2add" id="etime2add">
+                                <option value="">Select</option>
+                                <?php foreach ($every_10_minutes as $key_time => $value) : ?>
+                                <option value="<?=$key_time?>"><?=$value?></option>
+                                <?php endforeach; ?>
+                            </select>                                
+                          </div>
+                        </div>
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label for="" class="col-sm-4 control-label">Memo</label>
+                    <div class="col-sm-8">
+                      <textarea class="form-control" rows="3" name="memo2add" id="memo2add"></textarea>
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <div class="col-sm-offset-4 col-sm-8">
+                      <button type="submit" class="btn btn-default">Save</button>
+                      <button type="button" class="btn btn-default">Cancel</button>
+                    </div>
+                  </div>
+                </form>            
+            </div>           
         </div>
     </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-primary">Save</button>
-        <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
-        
-      </div>
-    </div>
-  </div>
 </div>
-  <script src="js/jquery-1.10.2.js"></script>
-  <script src="js/jquery-ui.js"></script>  
+    <script type="text/javascript">
+        jQuery(document).ready(function($){
+            $(".time_select_box").chosen();
+            $('.alert').hide();
+
+            $('#add_manual_time_f').submit(function(){
+                var stime2add = $('#stime2add').val();
+                var etime2add = $('#etime2add').val();
+                var date2add = $('#date2add').val();
+                var memo2add = $('#memo2add').val();
+                var project_id = $('#project_id').val();
+                var user_id = $('#user_id').val();      
+                if(stime2add == '' || etime2add == '') {
+                    $('.alert-warning').html("Please select start time and stop time");
+                    $('.alert-warning').show();
+                    setTimeout(function(){
+                        $('.alert').fadeOut();
+                    },3000);
+                    return false;
+                }
+                $.ajax({
+                   url: '<?= $vpath; ?>ajax_action.php',
+                   data: {action: 'add_manual_time', stime2add: stime2add, etime2add: etime2add, date2add: date2add, memo2add: memo2add, project_id: project_id, user_id: user_id},
+                   success: function(data) {
+                      var result = JSON.parse(data);
+                      if(result.error) {
+                        $('.alert-warning').html(result.error);
+                        $('.alert-warning').show();
+                      }
+                      if(result.success) {
+                        $('.alert-success').html(result.success);
+                        $('.alert-success').show();
+                      }
+                        setTimeout(function(){
+                            $('.alert').fadeOut();
+                        },3000);
+                   }
+                });
+                return false;
+            })
+            
+            function centerModal() {
+                $(this).css('display', 'block');
+                var $dialog = $(this).find(".modal-dialog");
+                var offset = ($(window).height() - $dialog.height()) / 2;
+                // Center modal vertically in window
+                $dialog.css("margin-top", offset);
+            }
+
+            $('.modal').on('show.bs.modal', centerModal);
+            $(window).on("resize", function () {
+                $('.modal:visible').each(centerModal);
+            });
+        })        
+
+    </script>
+    <script src="js/jquery-1.10.2.js"></script>
+    <script src="js/jquery-ui.js"></script>
+    <script>
+      $(function() {
+        $( "#datepicker" ).datepicker( "option", "dateFormat", "D, M dd, yy" ); 
+        $( "#datepicker" ).datepicker( "setDate", "<?=$current_date?>" );
+        $('#date2add').val($('#datepicker').val());
+        $('.date2add').html($('#datepicker').val());
+        $( "#datepicker" ).on('change', function(){
+            $('#date2add').val($(this).val());
+            $('.date2add').html($(this).val());
+        })
+      });
+    </script>
 <?php include 'includes/footer.php'; ?>
