8244d584035400276dd7040083e684f2ad1cd3f5
Curanh <ndthanh.it@gmail.com>
2014-10-24 21:36:46 +0700

    * Test time tracker

--- time_tracker/index.php
+++ time_tracker/index.php
@@ -135,7 +135,7 @@ if ($ACT == "prowork") {
             }
         }
 
-        $sql = "INSERT INTO serv_project_tracker (`project_id`, `worker_id`, `start_time`, `stop_time`, `note`, `work_type`) VALUES ('$project_id', '$user_id', NOW(), NOW(), '$note', '$type');";
+        $sql = "INSERT INTO serv_project_tracker (`project_id`, `worker_id`, `start_time`, `stop_time`, `note`, `work_type`) VALUES ('$project_id', '$user_id', NOW(), NOW(), '$note', 'H');";
         run_quary($sql);
         $idd = mysql_insert_id();
         if ($idd != "") {
@@ -194,12 +194,12 @@ if ($ACT == "uploadSnap") {
     if ($projectwork_id != "") {
         $rcheck = mysql_fetch_array(mysql_query("SELECT *,TIME_TO_SEC(TIMEDIFF(NOW(),stop_time)) AS wt FROM serv_project_tracker WHERE id=".$projectwork_id));
         $rcheckfirst = mysql_fetch_array(mysql_query("SELECT *,TIME_TO_SEC(TIMEDIFF(start_time,stop_time)) AS wt FROM serv_project_tracker WHERE id=".$projectwork_id));
-        // mysql_query("INSERT INTO table_debug (text_debug) VALUES ('".$rcheck['wt']."')");
+        
         $pj_id = getProjectID($projectwork_id);
         mysql_query("INSERT INTO table_debug (text_debug) VALUES ('projectword_id: ".$projectwork_id."')");
-        mysql_query("INSERT INTO table_debug (text_debug) VALUES ('".var_dump($pj_id=="")."')");
-        mysql_query("INSERT INTO table_debug (text_debug) VALUES ('check first ".$rcheckfirst['wt']."')");
-        if($rcheck['wt'] >= 600 || $rcheck['wt'] == "") {
+        mysql_query("INSERT INTO table_debug (text_debug) VALUES ('phj_id: ".$pj_id=."')");
+        mysql_query("INSERT INTO table_debug (text_debug) VALUES ('check first: ".$rcheckfirst['wt']."')");
+        if($rcheck['wt'] >= 600 || !$rcheckfirst['wt']) {
             
             $sql = "UPDATE `serv_project_tracker` SET  `stop_time`=NOW() WHERE `project_id` ='".$pj_id."'";
             run_quary($sql);
